ec25a999f27a3cbbd6eb27725c215d9ee2183da8
Radek Liebzeit <lie78@vsb.cz>
2015-05-26 08:15:25 +0200

    MISC-65: ResBill: vytvoření systému pro datové centrum

--- resbill/src/main/java/cz/vsb/resbill/util/WebUtils.java
+++ resbill/src/main/java/cz/vsb/resbill/util/WebUtils.java
@@ -8,23 +8,24 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 
-
 /**
  * @author Ing. Radek Liebzeit <radek.liebzeit@vsb.cz>
  *
  */
 public class WebUtils {
+
 	/**
+	 * Prida globalni chybu
 	 * 
-	 * @param model
-	 * @param objectName
-	 * @param msgKey
+	 * @param model mapa modelovych objektu
+	 * @param object Name jmeno objektu, u nejz doslo k chybe. Klic musi byt dopredu zavedeny v modelu
+	 * @param msgKey Klic zpravy z properties souboru
 	 */
 	public static void addGlobalError(ModelMap model, String objectName, String msgKey) {
-		BindingResult bindingResult = (BindingResult) model.get(BindingResult.MODEL_KEY_PREFIX + "dailyImport");
+		BindingResult bindingResult = (BindingResult) model.get(BindingResult.MODEL_KEY_PREFIX + objectName);
 		if (bindingResult == null) {
-			bindingResult = new BeanPropertyBindingResult(model.get(objectName), "dailyImport");
-			model.put(BindingResult.MODEL_KEY_PREFIX + "dailyImport", bindingResult);
+			bindingResult = new BeanPropertyBindingResult(model.get(objectName), objectName);
+			model.put(BindingResult.MODEL_KEY_PREFIX + objectName, bindingResult);
 		}
 		bindingResult.reject(msgKey);
 	}
