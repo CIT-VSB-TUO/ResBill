11c03129e7c432ee567efd1cbe35d253c4a9812f
Radek Liebzeit <lie78@vsb.cz>
2015-06-04 17:07:15 +0200

    RES-8: Dashboard - implementovat přehledy

--- resbill/src/main/resources/testData.sql
+++ resbill/src/main/resources/testData.sql
@@ -13,7 +13,7 @@ INSERT INTO "person" VALUES (2, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'pe
 INSERT INTO "customer" VALUES (1, 0, NULL, 'Zákazník 1', NULL, 1);
 INSERT INTO "customer" VALUES (2, 0, NULL, 'Zákazník 2', NULL, 2);
 
-INSERT INTO "contract" VALUES (1, 0, 0.00, 0, 'Kontrakt1', NULL, '2015-04-01', NULL, 1);
+INSERT INTO "contract" VALUES (1, 0, -110.00, 0, 'Kontrakt1', NULL, '2015-04-01', NULL, 1);
 INSERT INTO "contract" VALUES (2, 0, 0.00, 1, 'Fakulta strojní', NULL, '2015-04-01', NULL, 2);
 INSERT INTO "contract" VALUES (3, 0, 0.00, 2, 'Bez tarifu', NULL, '2015-04-01', NULL, 2);
 INSERT INTO "contract" VALUES (4, 0, 0.00, 3, 'Spotřeba po ukončení', NULL, '2014-01-01', '2014-01-31', 2);
