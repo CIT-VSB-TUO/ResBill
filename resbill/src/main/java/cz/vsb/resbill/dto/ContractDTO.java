61678e6b192383129cbfaa33f2452f904ce0618e
Radek Liebzeit <lie78@vsb.cz>
2015-06-10 07:59:52 +0200

    RES-8: Dashboard - implementovat přehledy

--- resbill/src/main/java/cz/vsb/resbill/dto/ContractDTO.java
+++ resbill/src/main/java/cz/vsb/resbill/dto/ContractDTO.java
@@ -15,85 +15,85 @@ import cz.vsb.resbill.util.ToStringBuilder;
  */
 public class ContractDTO implements Serializable {
 
-	/**
+  /**
    * 
    */
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private Integer contractId = null;
+  private Integer           contractId       = null;
 
-	private Long evidenceNumber = null;
+  private Long              evidenceNumber   = null;
 
-	private String name = null;
+  private String            name             = null;
 
-	/**
-	 * 
-	 * @param contract
-	 */
-	public void fill(Contract contract) {
-		contractId = contract.getId();
-		evidenceNumber = contract.getEvidenceNumber();
-		name = contract.getName();
-	}
+  /**
+   * 
+   * @param contract
+   */
+  public void fill(Contract contract) {
+    contractId = contract.getId();
+    evidenceNumber = contract.getEvidenceNumber();
+    name = contract.getName();
+  }
 
-	/**
-	 * @return the contractId
-	 */
-	public Integer getContractId() {
-		return contractId;
-	}
+  /**
+   * @return the contractId
+   */
+  public Integer getContractId() {
+    return contractId;
+  }
 
-	/**
-	 * @param contractId
-	 *          the contractId to set
-	 */
-	public void setContractId(Integer contractId) {
-		this.contractId = contractId;
-	}
+  /**
+   * @param contractId
+   *          the contractId to set
+   */
+  public void setContractId(Integer contractId) {
+    this.contractId = contractId;
+  }
 
-	/**
-	 * @return the evidenceNumber
-	 */
-	public Long getEvidenceNumber() {
-		return evidenceNumber;
-	}
+  /**
+   * @return the evidenceNumber
+   */
+  public Long getEvidenceNumber() {
+    return evidenceNumber;
+  }
 
-	/**
-	 * @param evidenceNumber
-	 *          the evidenceNumber to set
-	 */
-	public void setEvidenceNumber(Long evidenceNumber) {
-		this.evidenceNumber = evidenceNumber;
-	}
+  /**
+   * @param evidenceNumber
+   *          the evidenceNumber to set
+   */
+  public void setEvidenceNumber(Long evidenceNumber) {
+    this.evidenceNumber = evidenceNumber;
+  }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
 
-	/**
-	 * @param name
-	 *          the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+  /**
+   * @param name
+   *          the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		ToStringBuilder builder = new ToStringBuilder(this);
-		builder.append("contractId", contractId);
-		builder.append("evidenceNumber", evidenceNumber);
-		builder.append("name", name);
-		builder.append("toString()", super.toString());
-		return builder.toString();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    ToStringBuilder builder = new ToStringBuilder(this);
+    builder.append("contractId", contractId);
+    builder.append("evidenceNumber", evidenceNumber);
+    builder.append("name", name);
+    builder.append("toString()", super.toString());
+    return builder.toString();
+  }
 
 }
