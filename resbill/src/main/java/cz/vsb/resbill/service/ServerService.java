b8a33aade0b60439418d1fc23a0784eb538e34d0
Aleš Haladej <hal191@vsb.cz>
2015-06-24 11:28:38 +0200

    RES-21; Servery: Doupravit záložky Editace a Kontrakty po zavedení záložky Přehled
    drobne upravy

--- resbill/src/main/java/cz/vsb/resbill/service/ServerService.java
+++ resbill/src/main/java/cz/vsb/resbill/service/ServerService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import cz.vsb.resbill.criteria.ServerCriteria;
 import cz.vsb.resbill.dto.agenda.ServerAgendaDTO;
-import cz.vsb.resbill.dto.server.ServerDTO;
 import cz.vsb.resbill.dto.server.ServerEditDTO;
 import cz.vsb.resbill.dto.server.ServerHeaderDTO;
 import cz.vsb.resbill.dto.server.ServerListDTO;
@@ -21,76 +20,72 @@ import cz.vsb.resbill.model.Server;
  */
 public interface ServerService {
 
-  /**
-   * Finds a {@link Server} with given key.
-   * 
-   * @param serverId
-   *          primary key of a {@link Server}
-   * @return found {@link Server}, otherwise <code>null</code>
-   * @throws ResBillException
-   *           if unexpected error occurs
-   */
-  Server findServer(Integer serverId) throws ResBillException;
+	/**
+	 * Finds a {@link Server} with given key.
+	 * 
+	 * @param serverId
+	 *          primary key of a {@link Server}
+	 * @return found {@link Server}, otherwise <code>null</code>
+	 * @throws ResBillException
+	 *           if unexpected error occurs
+	 */
+	Server findServer(Integer serverId) throws ResBillException;
 
-  ServerDTO findServerDTO(Integer serverId) throws ResBillException;
+	ServerHeaderDTO findServerHeaderDTO(Integer serverId) throws ResBillException;
 
-  ServerHeaderDTO findServerHeaderDTO(Integer serverId) throws ResBillException;
+	ServerOverviewDTO findServerOverviewDTO(Integer serverId) throws ResBillException;
 
-  ServerOverviewDTO findServerOverviewDTO(Integer serverId) throws ResBillException;
+	/**
+	 * Finds a {@link ServerEditDTO} for given key.
+	 * 
+	 * @param serverId
+	 *          primary key of a {@link Server}
+	 * @return found {@link ServerEditDTO}, otherwise <code>null</code>
+	 * @throws ResBillException
+	 *           if unexpected error occurs
+	 */
+	ServerEditDTO findServerEditDTO(Integer serverId) throws ResBillException;
 
-  /**
-   * Finds a {@link ServerEditDTO} for given key.
-   * 
-   * @param serverId
-   *          primary key of a {@link Server}
-   * @return found {@link ServerEditDTO}, otherwise <code>null</code>
-   * @throws ResBillException
-   *           if unexpected error occurs
-   */
-  ServerEditDTO findServerEditDTO(Integer serverId) throws ResBillException;
+	/**
+	 * Finds a list of {@link Server} entities according to given criteria.
+	 * 
+	 * @param criteria
+	 *          filtering criteria
+	 * @param offset
+	 * @param limit
+	 * @return list of found {@link Server} entities
+	 * @throws ResBillException
+	 *           if unexpected error occurs
+	 */
+	List<Server> findServers(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
 
-  /**
-   * Finds a list of {@link Server} entities according to given criteria.
-   * 
-   * @param criteria
-   *          filtering criteria
-   * @param offset
-   * @param limit
-   * @return list of found {@link Server} entities
-   * @throws ResBillException
-   *           if unexpected error occurs
-   */
-  List<Server> findServers(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
+	List<ServerListDTO> findServerListDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
 
-  List<ServerDTO> findServerDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
+	List<ServerAgendaDTO> findServerAgendaDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
 
-  List<ServerListDTO> findServerListDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
+	/**
+	 * Persists given {@link Server} entity.
+	 * 
+	 * @param server
+	 *          entity to save
+	 * @return saved {@link Server} entity (with generated primary key)
+	 * @throws ServerServiceException
+	 *           if specific error occurs
+	 * @throws ResBillException
+	 *           if unexpected error occurs
+	 */
+	Server saveServer(Server server) throws ServerServiceException, ResBillException;
 
-  List<ServerAgendaDTO> findServerAgendaDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException;
-
-  /**
-   * Persists given {@link Server} entity.
-   * 
-   * @param server
-   *          entity to save
-   * @return saved {@link Server} entity (with generated primary key)
-   * @throws ServerServiceException
-   *           if specific error occurs
-   * @throws ResBillException
-   *           if unexpected error occurs
-   */
-  Server saveServer(Server server) throws ServerServiceException, ResBillException;
-
-  /**
-   * Deletes {@link Server} entity with given primary key.
-   * 
-   * @param serverId
-   *          key of deleted {@link Server} entity
-   * @return deleted {@link Server} entity
-   * @throws ServerServiceException
-   *           if specific error occurs
-   * @throws ResBillException
-   *           if unexpected error occurs
-   */
-  Server deleteServer(Integer serverId) throws ServerServiceException, ResBillException;
+	/**
+	 * Deletes {@link Server} entity with given primary key.
+	 * 
+	 * @param serverId
+	 *          key of deleted {@link Server} entity
+	 * @return deleted {@link Server} entity
+	 * @throws ServerServiceException
+	 *           if specific error occurs
+	 * @throws ResBillException
+	 *           if unexpected error occurs
+	 */
+	Server deleteServer(Integer serverId) throws ServerServiceException, ResBillException;
 }
--- resbill/src/main/java/cz/vsb/resbill/service/impl/ServerServiceImpl.java
+++ resbill/src/main/java/cz/vsb/resbill/service/impl/ServerServiceImpl.java
@@ -20,7 +20,6 @@ import cz.vsb.resbill.dao.ServerDAO;
 import cz.vsb.resbill.dto.ContractDTO;
 import cz.vsb.resbill.dto.DailyUsageDTO;
 import cz.vsb.resbill.dto.agenda.ServerAgendaDTO;
-import cz.vsb.resbill.dto.server.ServerDTO;
 import cz.vsb.resbill.dto.server.ServerEditDTO;
 import cz.vsb.resbill.dto.server.ServerHeaderDTO;
 import cz.vsb.resbill.dto.server.ServerListDTO;
@@ -43,299 +42,261 @@ import cz.vsb.resbill.service.ServerService;
 @ResBillService
 public class ServerServiceImpl implements ServerService {
 
-  private static final Logger log = LoggerFactory.getLogger(ServerServiceImpl.class);
+	private static final Logger log = LoggerFactory.getLogger(ServerServiceImpl.class);
 
-  @Inject
-  private ServerDAO           serverDAO;
+	@Inject
+	private ServerDAO serverDAO;
 
-  @Inject
-  private ContractServerDAO   contractServerDAO;
+	@Inject
+	private ContractServerDAO contractServerDAO;
 
-  @Inject
-  private DailyUsageDAO       dailyUsageDAO;
+	@Inject
+	private DailyUsageDAO dailyUsageDAO;
 
-  /**
+	/**
 	 * 
 	 */
-  @Override
-  public Server findServer(Integer serverId) throws ResBillException {
-    try {
-      return serverDAO.findServer(serverId);
-    } catch (Exception e) {
-      log.error("An unexpected error occured while finding Server by id=" + serverId, e);
-      throw new ResBillException(e);
-    }
-  }
+	@Override
+	public Server findServer(Integer serverId) throws ResBillException {
+		try {
+			return serverDAO.findServer(serverId);
+		} catch (Exception e) {
+			log.error("An unexpected error occured while finding Server by id=" + serverId, e);
+			throw new ResBillException(e);
+		}
+	}
 
-  /**
-	 * 
-	 */
-  public ServerDTO findServerDTO(Integer serverId) throws ResBillException {
-    try {
-      Server server = findServer(serverId);
-
-      return new ServerDTO(server);
-    } catch (ResBillException e) {
-      throw e;
-    } catch (Exception e) {
-      log.error("An unexpected error occured while finding ServerDTO by server.id=" + serverId, e);
-      throw new ResBillException(e);
-    }
-  }
-
-  /**
+	/**
    * 
    */
-  public ServerHeaderDTO findServerHeaderDTO(Integer serverId) throws ResBillException {
-    try {
-      Server server = findServer(serverId);
+	public ServerHeaderDTO findServerHeaderDTO(Integer serverId) throws ResBillException {
+		try {
+			Server server = findServer(serverId);
 
-      return new ServerHeaderDTO(server);
-    } catch (ResBillException e) {
-      throw e;
-    } catch (Exception e) {
-      log.error("An unexpected error occured while finding ServerHeaderDTO by server.id=" + serverId, e);
-      throw new ResBillException(e);
-    }
-  }
+			return new ServerHeaderDTO(server);
+		} catch (ResBillException e) {
+			throw e;
+		} catch (Exception e) {
+			log.error("An unexpected error occured while finding ServerHeaderDTO by server.id=" + serverId, e);
+			throw new ResBillException(e);
+		}
+	}
 
-  /**
+	/**
    * 
    */
-  public ServerOverviewDTO findServerOverviewDTO(Integer serverId) throws ResBillException {
-    try {
-      Server server = findServer(serverId);
+	public ServerOverviewDTO findServerOverviewDTO(Integer serverId) throws ResBillException {
+		try {
+			Server server = findServer(serverId);
 
-      ServerOverviewDTO dto = new ServerOverviewDTO(server);
+			ServerOverviewDTO dto = new ServerOverviewDTO(server);
 
-      DailyUsage lastDailyUsage = dailyUsageDAO.findServerLastDailyUsage(server.getId());
-      if (lastDailyUsage != null) {
-        DailyUsageDTO lastDailyUsageDTO = new DailyUsageDTO(lastDailyUsage);
-        dto.setLastDailyUsageDTO(lastDailyUsageDTO);
-      }
+			DailyUsage lastDailyUsage = dailyUsageDAO.findServerLastDailyUsage(server.getId());
+			if (lastDailyUsage != null) {
+				DailyUsageDTO lastDailyUsageDTO = new DailyUsageDTO(lastDailyUsage);
+				dto.setLastDailyUsageDTO(lastDailyUsageDTO);
+			}
 
-      ContractServer currentContractServer = contractServerDAO.findCurrentContractServer(serverId);
-      if (currentContractServer != null) {
-        dto.fill(currentContractServer);
-        ContractDTO currentContractDTO = new ContractDTO(currentContractServer.getContract());
-        dto.setCurrentContractDTO(currentContractDTO);
-      }
+			ContractServer currentContractServer = contractServerDAO.findCurrentContractServer(serverId);
+			if (currentContractServer != null) {
+				dto.fill(currentContractServer);
+				ContractDTO currentContractDTO = new ContractDTO(currentContractServer.getContract());
+				dto.setCurrentContractDTO(currentContractDTO);
+			}
 
-      return dto;
-    } catch (ResBillException e) {
-      throw e;
-    } catch (Exception e) {
-      log.error("An unexpected error occured while finding ServerOverviewDTO by server.id=" + serverId, e);
-      throw new ResBillException(e);
-    }
-  }
+			return dto;
+		} catch (ResBillException e) {
+			throw e;
+		} catch (Exception e) {
+			log.error("An unexpected error occured while finding ServerOverviewDTO by server.id=" + serverId, e);
+			throw new ResBillException(e);
+		}
+	}
 
-  /**
+	/**
 	 * 
 	 */
-  @Override
-  public ServerEditDTO findServerEditDTO(Integer serverId) throws ResBillException {
-    try {
-      Server server = findServer(serverId);
-      ContractServer cs = contractServerDAO.findCurrentContractServer(serverId);
+	@Override
+	public ServerEditDTO findServerEditDTO(Integer serverId) throws ResBillException {
+		try {
+			Server server = findServer(serverId);
+			ContractServer cs = contractServerDAO.findCurrentContractServer(serverId);
 
-      return new ServerEditDTO(server, cs);
-    } catch (ResBillException e) {
-      throw e;
-    } catch (Exception e) {
-      log.error("An unexpected error occured while finding ServerEditDTO by server.id=" + serverId, e);
-      throw new ResBillException(e);
-    }
-  }
+			return new ServerEditDTO(server, cs);
+		} catch (ResBillException e) {
+			throw e;
+		} catch (Exception e) {
+			log.error("An unexpected error occured while finding ServerEditDTO by server.id=" + serverId, e);
+			throw new ResBillException(e);
+		}
+	}
 
-  /**
+	/**
 	 * 
 	 */
-  @Override
-  public List<Server> findServers(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
-    try {
-      return serverDAO.findServers(criteria, offset, limit);
-    } catch (Exception e) {
-      log.error("An unexpected error occured while searching for Server entities by criteria: " + criteria, e);
-      throw new ResBillException(e);
-    }
-  }
+	@Override
+	public List<Server> findServers(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
+		try {
+			return serverDAO.findServers(criteria, offset, limit);
+		} catch (Exception e) {
+			log.error("An unexpected error occured while searching for Server entities by criteria: " + criteria, e);
+			throw new ResBillException(e);
+		}
+	}
 
-  /**
+	/**
    * 
    */
-  @Override
-  public List<ServerDTO> findServerDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
-    try {
-      List<Server> servers = findServers(criteria, offset, limit);
-      List<ServerDTO> dtos = new ArrayList<ServerDTO>();
+	@Override
+	public List<ServerListDTO> findServerListDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
+		try {
+			List<Object[]> fieldsList = serverDAO.findServersForList(criteria, offset, limit);
+			List<ServerListDTO> dtos = new ArrayList<ServerListDTO>();
 
-      for (Server server : servers) {
-        ServerDTO dto = new ServerDTO();
-        dto.fill(server);
-        dtos.add(dto);
-      }
+			for (Object[] fields : fieldsList) {
+				ServerListDTO dto = new ServerListDTO(fields);
+				dtos.add(dto);
+			}
 
-      return dtos;
-    } catch (Exception exc) {
-      log.error("An unexpected error occured while finding ServerDTOs.", exc);
-      throw new ResBillException(exc);
-    }
-  }
+			return dtos;
+		} catch (Exception exc) {
+			log.error("An unexpected error occured while finding ServerListDTOs.", exc);
+			throw new ResBillException(exc);
+		}
+	}
 
-  /**
-   * 
-   */
-  @Override
-  public List<ServerListDTO> findServerListDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
-    try {
-      List<Object[]> fieldsList = serverDAO.findServersForList(criteria, offset, limit);
-      List<ServerListDTO> dtos = new ArrayList<ServerListDTO>();
+	/**
+	 * Nalezne servery pro pouziti na Dashboardu (Agenda).
+	 * 
+	 * Tj. nalezne vsechny servery, ktere splnuji alespon jednu pozadovanou vlastnost (Feature) - tim se lisi od standardni metody findServers.
+	 * 
+	 * @param criteria
+	 * @param offset
+	 * @param limit
+	 * @return
+	 */
+	@Override
+	public List<ServerAgendaDTO> findServerAgendaDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
+		try {
+			ServerCriteria crit = null;
+			List<Server> servers = null;
+			Set<Integer> serverIds = new HashSet<Integer>();
 
-      for (Object[] fields : fieldsList) {
-        ServerListDTO dto = new ServerListDTO(fields);
-        dtos.add(dto);
-      }
+			// Servery odebírající zdroje mimo období pokryte kontrakty
+			crit = criteria.clone();
+			crit.setFeatures(EnumSet.of(ServerCriteria.Feature.DAILY_USAGE_OUT_OF_CONTRACT));
+			servers = findServers(crit, null, null);
+			Set<Integer> outOfContractServerIds = new HashSet<Integer>();
+			for (Server server : servers) {
+				outOfContractServerIds.add(server.getId());
+			}
+			serverIds.addAll(outOfContractServerIds);
 
-      return dtos;
-    } catch (Exception exc) {
-      log.error("An unexpected error occured while finding ServerListDTOs.", exc);
-      throw new ResBillException(exc);
-    }
-  }
+			// Servery bez kontraktu
+			crit = criteria.clone();
+			crit.setFeatures(EnumSet.of(ServerCriteria.Feature.NO_CONTRACT));
+			servers = findServers(crit, null, null);
+			Set<Integer> noContractServerIds = new HashSet<Integer>();
+			for (Server server : servers) {
+				noContractServerIds.add(server.getId());
+			}
+			serverIds.addAll(noContractServerIds);
 
-  /**
-   * Nalezne servery pro pouziti na Dashboardu (Agenda).
-   * 
-   * Tj. nalezne vsechny servery, ktere splnuji alespon jednu pozadovanou vlastnost (Feature) - tim se lisi od standardni metody findServers.
-   * 
-   * @param criteria
-   * @param offset
-   * @param limit
-   * @return
-   */
-  @Override
-  public List<ServerAgendaDTO> findServerAgendaDTOs(ServerCriteria criteria, Integer offset, Integer limit) throws ResBillException {
-    try {
-      ServerCriteria crit = null;
-      List<Server> servers = null;
-      Set<Integer> serverIds = new HashSet<Integer>();
+			// Servery bez denniho vyuziti zdroju
+			crit = criteria.clone();
+			crit.setFeatures(EnumSet.of(ServerCriteria.Feature.NO_DAILY_USAGE));
+			servers = findServers(crit, null, null);
+			Set<Integer> noDailyUsagesServerIds = new HashSet<Integer>();
+			for (Server server : servers) {
+				noDailyUsagesServerIds.add(server.getId());
+			}
+			serverIds.addAll(noDailyUsagesServerIds);
 
-      // Servery odebírající zdroje mimo období pokryte kontrakty
-      crit = criteria.clone();
-      crit.setFeatures(EnumSet.of(ServerCriteria.Feature.DAILY_USAGE_OUT_OF_CONTRACT));
-      servers = findServers(crit, null, null);
-      Set<Integer> outOfContractServerIds = new HashSet<Integer>();
-      for (Server server : servers) {
-        outOfContractServerIds.add(server.getId());
-      }
-      serverIds.addAll(outOfContractServerIds);
+			// Pokud neexistuje zadny "zajimavy" kontrakt, pak vratim prazdny seznam
+			if (serverIds.isEmpty()) {
+				return new ArrayList<ServerAgendaDTO>();
+			}
 
-      // Servery bez kontraktu
-      crit = criteria.clone();
-      crit.setFeatures(EnumSet.of(ServerCriteria.Feature.NO_CONTRACT));
-      servers = findServers(crit, null, null);
-      Set<Integer> noContractServerIds = new HashSet<Integer>();
-      for (Server server : servers) {
-        noContractServerIds.add(server.getId());
-      }
-      serverIds.addAll(noContractServerIds);
+			// Ziskam vsechny "zajimave" kontrakty setridene dle puvodniho pozadavku
+			crit = new ServerCriteria();
+			crit.setServerIds(serverIds);
+			crit.setOrderBy(criteria.getOrderBy());
 
-      // Servery bez denniho vyuziti zdroju
-      crit = criteria.clone();
-      crit.setFeatures(EnumSet.of(ServerCriteria.Feature.NO_DAILY_USAGE));
-      servers = findServers(crit, null, null);
-      Set<Integer> noDailyUsagesServerIds = new HashSet<Integer>();
-      for (Server server : servers) {
-        noDailyUsagesServerIds.add(server.getId());
-      }
-      serverIds.addAll(noDailyUsagesServerIds);
+			servers = findServers(crit, offset, limit);
+			List<ServerAgendaDTO> dtos = new ArrayList<ServerAgendaDTO>();
 
-      // Pokud neexistuje zadny "zajimavy" kontrakt, pak vratim prazdny seznam
-      if (serverIds.isEmpty()) {
-        return new ArrayList<ServerAgendaDTO>();
-      }
+			for (Server server : servers) {
+				ServerAgendaDTO dto = new ServerAgendaDTO();
+				dto.fill(server);
+				dtos.add(dto);
 
-      // Ziskam vsechny "zajimave" kontrakty setridene dle puvodniho pozadavku
-      crit = new ServerCriteria();
-      crit.setServerIds(serverIds);
-      crit.setOrderBy(criteria.getOrderBy());
+				if (outOfContractServerIds.contains(server.getId())) {
+					dto.setDailyUsagesOutOfContract(true);
+				}
+				if (noContractServerIds.contains(server.getId())) {
+					dto.setNoContract(true);
+				}
+				if (noDailyUsagesServerIds.contains(server.getId())) {
+					dto.setNoDailyUsage(true);
+				}
+			}
 
-      servers = findServers(crit, offset, limit);
-      List<ServerAgendaDTO> dtos = new ArrayList<ServerAgendaDTO>();
+			return dtos;
+		} catch (Exception exc) {
+			log.error("An unexpected error occured while finding ServerAgendaDTOs.", exc);
+			throw new ResBillException(exc);
+		}
+	}
 
-      for (Server server : servers) {
-        ServerAgendaDTO dto = new ServerAgendaDTO();
-        dto.fill(server);
-        dtos.add(dto);
-
-        if (outOfContractServerIds.contains(server.getId())) {
-          dto.setDailyUsagesOutOfContract(true);
-        }
-        if (noContractServerIds.contains(server.getId())) {
-          dto.setNoContract(true);
-        }
-        if (noDailyUsagesServerIds.contains(server.getId())) {
-          dto.setNoDailyUsage(true);
-        }
-      }
-
-      return dtos;
-    } catch (Exception exc) {
-      log.error("An unexpected error occured while finding ServerAgendaDTOs.", exc);
-      throw new ResBillException(exc);
-    }
-  }
-
-  /**
+	/**
 	 * 
 	 */
-  @Override
-  public Server saveServer(Server server) throws ServerServiceException, ResBillException {
-    try {
-      // kontrola jedinecnosti serverId
-      ServerCriteria criteria = new ServerCriteria();
-      criteria.setServerId(server.getServerId());
-      List<Server> servers = serverDAO.findServers(criteria, null, null);
-      Server foundServer = DataAccessUtils.singleResult(servers);
-      if (foundServer != null && !foundServer.getId().equals(server.getId())) {
-        throw new ServerServiceException(Reason.NONUNIQUE_SERVER_ID);
-      }
+	@Override
+	public Server saveServer(Server server) throws ServerServiceException, ResBillException {
+		try {
+			// kontrola jedinecnosti serverId
+			ServerCriteria criteria = new ServerCriteria();
+			criteria.setServerId(server.getServerId());
+			List<Server> servers = serverDAO.findServers(criteria, null, null);
+			Server foundServer = DataAccessUtils.singleResult(servers);
+			if (foundServer != null && !foundServer.getId().equals(server.getId())) {
+				throw new ServerServiceException(Reason.NONUNIQUE_SERVER_ID);
+			}
 
-      return serverDAO.saveServer(server);
-    } catch (ServerServiceException e) {
-      throw e;
-    } catch (Exception e) {
-      log.error("An unexpected error occured while saving Server entity: " + server, e);
-      throw new ResBillException(e);
-    }
-  }
+			return serverDAO.saveServer(server);
+		} catch (ServerServiceException e) {
+			throw e;
+		} catch (Exception e) {
+			log.error("An unexpected error occured while saving Server entity: " + server, e);
+			throw new ResBillException(e);
+		}
+	}
 
-  /**
+	/**
 	 * 
 	 */
-  @Override
-  public Server deleteServer(Integer serverId) throws ServerServiceException, ResBillException {
-    try {
-      Server server = serverDAO.findServer(serverId);
-      // kontrola zavislosti
-      // kontrakty
-      ContractServerCriteria csCriteria = new ContractServerCriteria();
-      csCriteria.setServerId(serverId);
-      List<ContractServer> css = contractServerDAO.findContractServers(csCriteria, null, null);
-      if (!css.isEmpty()) {
-        throw new ServerServiceException(Reason.CONTRACT_ASSOCIATED);
-      }
-      // denni pouziti
-      if (!server.getDailyUsages().isEmpty()) { // FIXME prepsat do DAO?
-        throw new ServerServiceException(Reason.DAILY_USAGE_EXISTENCE);
-      }
-      return serverDAO.deleteServer(server);
-    } catch (ServerServiceException e) {
-      throw e;
-    } catch (Exception e) {
-      log.error("An unexpected error occured while deleting Server with id=" + serverId, e);
-      throw new ResBillException(e);
-    }
-  }
+	@Override
+	public Server deleteServer(Integer serverId) throws ServerServiceException, ResBillException {
+		try {
+			Server server = serverDAO.findServer(serverId);
+			// kontrola zavislosti
+			// kontrakty
+			ContractServerCriteria csCriteria = new ContractServerCriteria();
+			csCriteria.setServerId(serverId);
+			List<ContractServer> css = contractServerDAO.findContractServers(csCriteria, null, null);
+			if (!css.isEmpty()) {
+				throw new ServerServiceException(Reason.CONTRACT_ASSOCIATED);
+			}
+			// denni pouziti
+			if (!server.getDailyUsages().isEmpty()) { // FIXME prepsat do DAO?
+				throw new ServerServiceException(Reason.DAILY_USAGE_EXISTENCE);
+			}
+			return serverDAO.deleteServer(server);
+		} catch (ServerServiceException e) {
+			throw e;
+		} catch (Exception e) {
+			log.error("An unexpected error occured while deleting Server with id=" + serverId, e);
+			throw new ResBillException(e);
+		}
+	}
 }
