4988eee0afcc6fbe2f41fb6da6205a22513b075d
Aleš Haladej <hal191@vsb.cz>
2015-06-26 11:35:41 +0200

    RES-6: Implementace autentizace a autorizace
    formular pro prihlaseni

--- resbill/src/main/java/cz/vsb/resbill/web/IndexController.java
+++ resbill/src/main/java/cz/vsb/resbill/web/IndexController.java
@@ -1,15 +1,33 @@
 package cz.vsb.resbill.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-  @RequestMapping(value = { "/", "/index" })
-  public String index() {
-    // return "index";
-    return "redirect:/reports/agenda";
-  }
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+	@RequestMapping({ "/", "/index" })
+	public String index() {
+		if (log.isDebugEnabled()) {
+			log.debug("Logged user: " + getLoggedUserName());
+		}
+		return "index";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	private String getLoggedUserName() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication != null ? authentication.getName() : null;
+	}
 
 }
